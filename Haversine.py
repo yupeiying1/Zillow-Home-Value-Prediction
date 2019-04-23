fc99557d2a44e1fb8b2c241c6466b030ca5f2531
Peiying Yu <py2244@columbia.edu>
2019-04-23 11:51:44 -0400

    lots of things completed

--- Haversine.py
+++ Haversine.py
@@ -7,7 +7,7 @@ def distance(origin, destination):
 
     dlat = math.radians(lat2-lat1)
     dlon = math.radians(lon2-lon1)
-    a = math.sin(dlat/2) * math.sin(dlat/2) + math.cos(math.radians(lat1))         * math.cos(math.radians(lat2)) * math.sin(dlon/2) * math.sin(dlon/2)
+    a = math.sin(dlat/2) * math.sin(dlat/2) + math.cos(math.radians(lat1)) * math.cos(math.radians(lat2)) * math.sin(dlon/2) * math.sin(dlon/2)
     c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
     d = radius * c
 
